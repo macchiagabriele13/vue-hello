520290c9272a79400ea76cdbf400cc4f4ac6db34
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-26 16:42:40 +0200

    Aggiunta bottone per generare immagine

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,14 @@ createApp({
   data() {
     return {
       title: 'Questo è il primo esercizio con Vue JS!',
-      randomImage: 'https://picsum.photo/400/200'
+      randomImage: 'https://picsum.photos/200'
     }
+  },
+  methods:{
+    generateImage(){
+        newImage = this.randomImage
+
+    }
+
   }
 }).mount('#app')
